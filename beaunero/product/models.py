83361badd389f232f8f835853ad323381c00a1e0
choisaywhy <sehwa98@icloud.com>
2021-05-13 17:47:40 +0900

    feat : Create product model

--- beaunero/product/models.py
+++ beaunero/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# from account.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+    key = models.IntegerField()
+    name = models.CharField(max_length = 100)
+    content = models.TextField()
+    picture = models.TextField()
+    
+    def __str__(self):
+        return self.name
